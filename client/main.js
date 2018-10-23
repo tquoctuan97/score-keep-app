7dcf9b2feabbfec92c9f568c3d34afc18c28bc2c
TuanTQ <tquoctuan97@gmail.com>
2018-10-23 09:34:52 +0700

    render static using map

--- client/main.js
+++ client/main.js
@@ -2,6 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
+const players = [{
+  _id: '1',
+  name: 'Lauren',
+  score: 99
+},{
+  _id: '2',
+  name: 'Cory',
+  score: -1
+},{
+  _id: '3',
+  name: 'Tuan',
+  score: -12
+}];
+
+const renderPlayers = function(playerList) {
+  return playerList.map(function(player){
+    return (
+      <p key={player._id}>
+        {player.name} has {player.score} point(s).
+      </p>
+    );
+  });
+}
+
 Meteor.startup(function(){
   let title = 'Score Keep';
   let name = 'Tuấn';
@@ -10,6 +34,7 @@ Meteor.startup(function(){
       <h1>{title}</h1>
       <p>Hello {name}</p>
       <p>This is my second p</p>
+      {renderPlayers(players)}
     </div>
   );
 
