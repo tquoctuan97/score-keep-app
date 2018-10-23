b655e1f94f3a74c5bbcf9acee30483beb43ebebd
TuanTQ <tquoctuan97@gmail.com>
2018-10-23 14:21:22 +0700

    mini-mongo-challenge

--- client/main.js
+++ client/main.js
@@ -1,49 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Meteor} from 'meteor/meteor';
-import {Players} from './../imports/api/players';
-import {Tracker} from 'meteor/tracker'
+import { Meteor } from 'meteor/meteor';
+import { Players } from './../imports/api/players';
+import { Tracker } from 'meteor/tracker';
 
-
-Tracker.autorun(function(){
-  console.log('Players list',Players.find().fetch());
-})
-
-const players = [{
-  _id: '1',
-  name: 'Lauren',
-  score: 99
-},{
-  _id: '2',
-  name: 'Cory',
-  score: -1
-},{
-  _id: '3',
-  name: 'Tuan',
-  score: -12
-}];
-
-const renderPlayers = function(playerList) {
-  return playerList.map(function(player){
+const renderPlayers = playerList => {
+  return playerList.map(player => {
     return (
       <p key={player._id}>
         {player.name} has {player.score} point(s).
       </p>
     );
   });
-}
+};
 
-Meteor.startup(function(){
-  let title = 'Score Keep';
-  let name = 'Tuấn';
-  let jsx = (
-    <div>
-      <h1>{title}</h1>
-      <p>Hello {name}</p>
-      <p>This is my second p</p>
-      {renderPlayers(players)}
-    </div>
-  );
+Meteor.startup(function() {
+  // Call tracker.autorun
+  // Create variable called players -> set equal to fetch query
+  // Render player to the screen
 
-  ReactDOM.render(jsx, document.getElementById('app'));
+  Tracker.autorun(function() {
+    let players = Players.find().fetch();
+    let title = 'Score Keep';
+    let name = 'Tuấn';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <p>Hello {name}</p>
+        <p>This is my second p</p>
+        {renderPlayers(players)}
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
+
+  //insert new doc into collection
+    Players.insert({
+      name: 'Hang',
+      score: 99
+  });
 });
--- server/main.js
+++ server/main.js
@@ -2,9 +2,9 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
 Meteor.startup(function(){
-    Players.insert({
-        name: 'Xuan',
-        score: 10
-    });
-    console.log(Players.find().fetch());
+    // Players.insert({
+    //     name: 'Xuan',
+    //     score: 10
+    // });
+    // console.log(Players.find().fetch());
 });
